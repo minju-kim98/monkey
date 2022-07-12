40739d3a9ea8f2372f462b8b6737386da9bd15c3
minju-kim98 <ducco705@snu.ac.kr>
2022-07-12 11:50:34 +0900

    parseProgram() completed

--- parser/parser.go
+++ parser/parser.go
@@ -22,11 +22,67 @@ func New(l *lexer.Lexer) *Parser {
 	return p
 }
 
+func (p *Parser) ParseProgram() *ast.Root {
+	root := &ast.Root{}
+	root.Statements = []ast.Statement{}
+
+	for !p.currTokenTypeIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			root.Statements = append(root.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return root
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.currToken.Type {
+	case token.LET:
+		return p.parseLetStmt()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStmt() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.currToken}
+
+	if !p.expectedPeekType(token.ID) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.currToken, Value: p.currToken.Literal}
+
+	if !p.expectedPeekType(token.ASSIGN) {
+		return nil
+	}
+
+	for !p.currTokenTypeIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
 func (p *Parser) nextToken() {
 	p.currToken = p.peekToken
 	p.peekToken = p.l.NextToken()
 }
 
-func (p *Parser) ParseProgram() *ast.Root {
-	return nil
+func (p *Parser) currTokenTypeIs(tt token.TokenType) bool {
+	return p.currToken.Type == tt
+}
+
+func (p *Parser) peekTokenTypeIs(tt token.TokenType) bool {
+	return p.peekToken.Type == tt
+}
+
+func (p *Parser) expectedPeekType(tt token.TokenType) bool {
+	if p.peekTokenTypeIs(tt) {
+		p.nextToken()
+		return true
+	}
+	return false
 }
